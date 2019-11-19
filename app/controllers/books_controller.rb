db4bf4f0a11f663e5a62a64f41af45424f80c797
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-11-19 10:11:55 -0300

    api setup

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  def index
+    render json: Book.order('title ASC')
+  end
+
+  def create
+    Book.create(book_params)
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+  end
+
+  private
+    def book_params
+      params.permit(:title, :category)
+    end
+end
